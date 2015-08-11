09d09a06cd04c67c7b02fb75bcdd2e6ef668262c
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-08-11 10:41:32 -0400

    Can't store similarity of an object to itself

--- src/django_recommend/models.py
+++ src/django_recommend/models.py
@@ -3,9 +3,10 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.db import models
 
 
 class ObjectSimilarity(models.Model):
@@ -25,3 +26,12 @@ class ObjectSimilarity(models.Model):
         unique_together = (
             'object_1_id', 'object_1_content_type',
             'object_2_id', 'object_2_content_type')
+
+    def clean(self):
+        if (self.object_1_id == self.object_2_id and
+                self.object_1_content_type == self.object_2_content_type):
+            raise ValidationError('An object cannot be similar to itself.')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(ObjectSimilarity, self).save(*args, **kwargs)
--- src/django_recommend/tests.py
+++ src/django_recommend/tests.py
@@ -3,6 +3,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from django.core.exceptions import ValidationError
 from django.contrib.contenttypes.models import ContentType
 from django.db import IntegrityError
 from django.test import TestCase
@@ -13,20 +14,33 @@ from . import models
 class ObjectSimilarityTest(TestCase):
     """Tests for the ObjectSimilarity model."""
 
+    def setUp(self):
+        self.ctype_a = ContentType.objects.create(app_label='foo')
+        self.ctype_b = ContentType.objects.create(app_label='bar')
+
     def test_duplicate_similarities(self):
         """Duplicate similarities can't be created."""
-        ctype_a = ContentType.objects.create(app_label='foo')
-        ctype_b = ContentType.objects.create(app_label='bar')
 
         # Creating one should work OK
         models.ObjectSimilarity.objects.create(
-            object_1_id=1, object_1_content_type=ctype_a,
-            object_2_id=1, object_2_content_type=ctype_b,
+            object_1_id=1, object_1_content_type=self.ctype_a,
+            object_2_id=1, object_2_content_type=self.ctype_b,
             score=3)
 
         # Creating the second should not (even with a different score)
-        with self.assertRaises(IntegrityError):
+        with self.assertRaises(ValidationError):
             models.ObjectSimilarity.objects.create(
-                object_1_id=1, object_1_content_type=ctype_a,
-                object_2_id=1, object_2_content_type=ctype_b,
+                object_1_id=1, object_1_content_type=self.ctype_a,
+                object_2_id=1, object_2_content_type=self.ctype_b,
                 score=4)
+
+    def test_similar_to_self(self):
+        """Objects can't be similar to themselves."""
+        with self.assertRaises(ValidationError) as exc:
+            models.ObjectSimilarity.objects.create(
+                object_1_id=1, object_1_content_type=self.ctype_a,
+                object_2_id=1, object_2_content_type=self.ctype_a,
+                score=4)
+
+        self.assertEqual(exc.exception.error_dict['__all__'][0].message,
+                         'An object cannot be similar to itself.')
