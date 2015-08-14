f0e5669ef8ca556651de419c5c707bde9475e356
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-08-14 15:38:02 -0400

    Distribute django_recommend.templatetags with pkg

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description='Generate recommendations in Django.',
     license='MIT',
     package_dir={'': 'src'},
-    packages=['django_recommend'],
+    packages=['django_recommend', 'django_recommend.templatetags'],
     install_requires=['django', 'pyrecommend'],
     setup_requires=['wheel'],
     long_description=read('README.rst'),
