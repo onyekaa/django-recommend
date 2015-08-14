003b45c2bb5f83ca091ae9435fd3fa122ed3e3f2
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-08-14 17:30:42 -0400

    More thoroughly test settings proxy

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -3,6 +3,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import pytest
+
 from django_recommend import conf
 
 
@@ -13,3 +15,27 @@ def test_proxies_django_settings(settings):
 
     settings.INSTALLED_APPS = ('bar',)
     assert conf.settings.INSTALLED_APPS == ('bar',)
+
+
+def test_provides_defaults(settings):
+    """Unset app settings get defaults."""
+    del settings.RECOMMEND_ENABLE_AUTOCALC
+
+    # Ensure setting is not accessible
+    assert not hasattr(settings, 'RECOMMEND_ENABLE_AUTOCALC')
+    assert conf.settings.RECOMMEND_ENABLE_AUTOCALC is True
+
+
+def test_user_overrides(settings):
+    """Users can override the defaults."""
+    settings.RECOMMEND_ENABLE_AUTOCALC = 'foobar'
+
+    assert conf.settings.RECOMMEND_ENABLE_AUTOCALC == 'foobar'
+
+
+def test_bogus_setting(settings):
+    """Nonexistant settings stil raise AttributeError."""
+    with pytest.raises(AttributeError):
+
+        # pylint: disable=pointless-statement
+        settings.THIS_ISNT_A_REAL_SETTING
