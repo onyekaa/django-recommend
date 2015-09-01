97f8ca94034e10cd4184d9365cf589ea0501e283
Daniel Passaro <daniel.passaro@regionalhelpwanted.com>
2015-09-01 14:00:08 -0400

    Release v0.1.dev7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='django-recommend',
-    version='0.1.dev6',
+    version='0.1.dev7',
     author='Dan Passaro',
     author_email='danpassaro@gmail.com',
     description='Generate recommendations in Django.',
